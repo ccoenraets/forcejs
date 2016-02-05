2c32690034d4d07b3b0fe6e477fc29c9bb315474
Tom Lagier <tom@rosetta.com>
2016-02-05 15:38:41 -0800

    Consistent variable names

--- force.js
+++ force.js
@@ -49,7 +49,7 @@ let // The login URL for the OAuth process
     useProxy = (window.cordova || window.SfdcApp || window.sforce) ? false : true,
     
     //Additional login window URL parameters for customizing the desktop login experience
-    loginURLParameters = {};
+    loginURLParams = {};
 
 /*
  * Determines the request base URL.
